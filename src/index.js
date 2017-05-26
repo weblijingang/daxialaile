4968195d9af6eece5dff86678c2c96c02659f466
lijingang <lijingang@daxia520.com>
2017-05-26 17:21:31 +0800

    babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "babel-polyfill";
+console.log(Array.from('aaa'))
+const square = n => n * n;
+console.log("====");
+const sum = (a,b)=>a+b;
+let x = sum(1,2);
+console.log(x);
+Object.assigin()
---
+++ src/jsx.js
@@ -0,0 +1,6 @@
+import React,{Component}from "react";
+class DemoComponent extends Component{
+  render(){
+    return <h1>hello world</h1>
+  }
+}
